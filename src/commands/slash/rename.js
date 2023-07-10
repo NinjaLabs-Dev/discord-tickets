9755ba2e0e169b998d50b2d0176adfd521201f59
NinjaLabs <fraser@ninjalabs.co.uk>
2023-07-10 01:42:01 +0100

    Add rename command & update close emoji

---
+++ src/commands/slash/rename.js
@@ -0,0 +1,105 @@
+const { SlashCommand } = require('@eartharoid/dbf');
+const { ApplicationCommandOptionType } = require('discord.js');
+const ExtendedEmbedBuilder = require('../../lib/embed');
+const { isStaff } = require('../../lib/users');
+const { logTicketEvent } = require('../../lib/logging');
+
+module.exports = class RenameCommand extends SlashCommand {
+	constructor(client, options) {
+		const name = 'rename';
+		super(client, {
+			...options,
+			description: client.i18n.getMessage(null, `commands.slash.${name}.description`),
+			descriptionLocalizations: client.i18n.getAllMessages(`commands.slash.${name}.description`),
+			dmPermission: false,
+			name,
+			nameLocalizations: client.i18n.getAllMessages(`commands.slash.${name}.name`),
+			options: [
+				{
+					name: 'name',
+					required: true,
+					type: ApplicationCommandOptionType.String,
+				},
+			].map(option => {
+				option.descriptionLocalizations = client.i18n.getAllMessages(`commands.slash.${name}.options.${option.name}.description`);
+				option.description = option.descriptionLocalizations['en-GB'];
+				option.nameLocalizations = client.i18n.getAllMessages(`commands.slash.${name}.options.${option.name}.name`);
+				return option;
+			}),
+		});
+	}
+
+	/**
+	 * @param {import("discord.js").ChatInputCommandInteraction} interaction
+	 */
+	async run(interaction) {
+		/** @type {import("client")} */
+		const client = this.client;
+
+		await interaction.deferReply();
+
+		const settings = await client.prisma.guild.findUnique({ where: { id: interaction.guild.id } });
+		const getMessage = client.i18n.getLocale(settings.locale);
+		const ticket = await client.prisma.ticket.findUnique({
+			include: { category: { select: { channelName: true } } },
+			where: { id: interaction.channel.id },
+		});
+
+		if (!ticket) {
+			return await interaction.editReply({
+				embeds: [
+					new ExtendedEmbedBuilder({
+						iconURL: interaction.guild.iconURL(),
+						text: settings.footer,
+					})
+						.setColor(settings.errorColour)
+						.setTitle(getMessage('misc.not_ticket.title'))
+						.setDescription(getMessage('misc.not_ticket.description')),
+				],
+			});
+		}
+
+		if (!(await isStaff(interaction.guild, interaction.user.id))) { // if user is not staff
+			return await interaction.editReply({
+				embeds: [
+					new ExtendedEmbedBuilder({
+						iconURL: interaction.guild.iconURL(),
+						text: ticket.guild.footer,
+					})
+						.setColor(ticket.guild.errorColour)
+						.setTitle(getMessage('commands.slash.move.not_staff.title'))
+						.setDescription(getMessage('commands.slash.move.not_staff.description')),
+				],
+			});
+		}
+
+		const name = interaction.options.getString('name', true);
+		const oldName = interaction.channel.name;
+		await interaction.channel.setName(name);
+
+		logTicketEvent(this.client, {
+			action: 'update',
+			diff: {
+				original: { name: oldName },
+				updated: { name: name },
+			},
+			target: {
+				id: ticket.id,
+				name: `<#${ticket.id}>`,
+			},
+			userId: interaction.user.id,
+		});
+
+		return await interaction.editReply({
+			embeds: [
+				new ExtendedEmbedBuilder({
+					iconURL: interaction.guild.iconURL(),
+					text: settings.footer,
+				})
+					.setColor(settings.successColour)
+					.setTitle(getMessage('commands.slash.rename.success.title'))
+					.setDescription(getMessage('commands.slash.rename.success.description', { name: name })),
+			],
+		});
+	}
+};
